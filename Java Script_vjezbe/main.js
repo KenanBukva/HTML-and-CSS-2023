4fe907e0c2e4e27e8552fd597e12844068d1282c
KenanBukva <kenan.bukva@gmail.com>
2023-02-16 18:39:01 +0100

    firtst commit after second lessons

---
+++ Java Script_vjezbe/main.js
@@ -0,0 +1,40 @@
+    document.write("<h2>" +new Date()+"<h2>");
+    document.write(5*5);
+    // tacka zarez ";" omgucava da vise linija serijski u jednu liniju koda stavimo kao ispod
+    document.write("</br>"); document.write("hello world");
+
+    console.log("ako zelimo da nesto ispisemo u konzoli");
+
+    document.write(100)
+    // vrsta podataka - cijeli brojevi npr 100
+    document.write("<br>")
+    document.write(10.55);
+    document.write("<br>")
+    document.write("Hello world helo world")
+       // navodnici u zagradi se kotiste kod string vrste podataka
+    document.write(true);
+    // binarna vrsta podataka true ili folse
+      
+    
+    // JEDNOLINISKI KOMENTAR
+    /*  VISELINIJSKI KOMENTRI */
+
+    var prviBroj= 100;
+    var drugiBroj=100.56;
+    var text ="Hello World";
+    var msg=" welcome";
+    var username="Kenan Bukva";
+    var upaljeno= true;
+    document.write("<br>")
+    document.write(msg+username);
+
+    // funkcija varijable 
+    document.write("<br>")
+
+    document.write(text)
+    document.write("<br>")
+    document.write(drugiBroj*2);
+    var a=12;
+    var b=133;
+    document.write(a*b);
+
